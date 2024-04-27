894dd220d11d57343bac5604f7f59cc56a03ed0b
David Furman <DavidAsherFurman@gmail.com>
2024-04-27 15:09:36 -0700

    Make swift-syntax version flexible

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax.git", "509.0.0"..<"511.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.4.0"),
     ],
     targets: [
