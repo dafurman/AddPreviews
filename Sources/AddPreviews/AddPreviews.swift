0dc414086dd3c2324bd4c8db1ff640a8af60971a
David Furman <DavidAsherFurman@gmail.com>
2024-04-28 13:10:37 -0700

    Remove IterableSequence typealias

--- Sources/AddPreviews/AddPreviews.swift
+++ Sources/AddPreviews/AddPreviews.swift
@@ -26,5 +26,3 @@
 @attached(member, names: named(previews), named(next), named(iterator))
 @attached(extension, conformances: CustomDebugStringConvertible)
 public macro AddPreviews() = #externalMacro(module: "AddPreviewsMacros", type: "AddPreviews")
-
-public typealias IterableSequence = Sequence & IteratorProtocol
