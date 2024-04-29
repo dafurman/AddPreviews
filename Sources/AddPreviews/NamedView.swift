4a5002f10ef48acac3117bc52fa6b674465340a2
David Furman <DavidAsherFurman@gmail.com>
2024-04-28 21:29:55 -0700

    Make NamedView conform to View

--- Sources/AddPreviews/NamedView.swift
+++ Sources/AddPreviews/NamedView.swift
@@ -2,12 +2,16 @@
 
 import SwiftUI
 
-public struct NamedView {
+public struct NamedView: View {
     public let name: String
     public let view: any View
 
     public init(name: String, view: any View) {
         self.name = name
         self.view = view
+    }
+
+    public var body: some View {
+        AnyView(view)
     }
 }
