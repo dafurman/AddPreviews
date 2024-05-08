f7639e4311e050edba2d1852dcb99c4ed93dc9c9
David Furman <DavidAsherFurman@gmail.com>
2024-05-07 22:51:19 -0700

    Delete SnapshottableView

--- Sources/AddPreviews/NamedView.swift
+++ Sources/AddPreviews/NamedView.swift
@@ -2,17 +2,6 @@
 
 import SwiftUI
 
-public protocol SnapshottableView: View {
-    var name: String { get }
-    var view: any View { get }
-}
-
-extension SnapshottableView {
-    public var body: some View {
-        AnyView(view)
-    }
-}
-
 public struct NamedView: SnapshottableView {
     public let name: String
     public let view: any View
